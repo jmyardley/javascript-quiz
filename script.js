c769560e8937cbb8cb00d8e787fdad2b199aedd2
James Y <35856061+jmyardley@users.noreply.github.com>
2020-11-06 01:24:02 -0500

    added questions and updated displayQuestion function

--- script.js
+++ script.js
@@ -7,21 +7,55 @@ var questionArea = document.querySelector("#questionList");
 
 var questions = [
     {
-        question: "what is JS?",
+        question: "what is JavaScript?",
         choices: [
+            "A national holiday",
+            "An archaeological discovery",
             "A language for the web",
-            "A drink"
+            "A coffee drink"
+        ],
+        answerIndex: 2
+    },
+    {
+        question: "what type of variable stores multiple objects?",
+        choices: [
+            "Function",
+            "Array",
+            "Jazzmaster",
+            "Shopping cart"
+        ],
+        answerIndex: 1
+    },
+    {
+        question: "What is the correct way to call the function myFunction?",
+        choices: [
+            "Avengers(assemble)",
+            "Ready, set, go",
+            "Hey Siri, run myFunction",
+            "myFunction()"
+        ],
+        answerIndex: 3
+    },
+    {
+        question: "Where would you place Javascript code in HTML?",
+        choices: [
+            "Inside <head></head> tags",
+            "Inside <script></script> tags",
+            "Inside the Oval Office",
+            "Inside the Salty Spittoon"
+        ],
+        answerIndex: 1
+    },
+    {
+        question: "Who invented Javascript?",
+        choices: [
+            "Brendan Eich",
+            "Jim Henson",
+            "God",
+            "Bernie Sanders"
         ],
         answerIndex: 0
     },
-    {
-        question: "what is CSS?",
-        choices: [
-            "A language for webpage appearance",
-            "A politician"
-        ],
-        answerIndex: 0
-    }
 ]
 
 //------------------- COUNTDOWN -------------------
@@ -47,11 +81,16 @@ startButton.addEventListener("click", function () {
 var currentQuestionIndex = 0;
 
 function displayQuestion() {
+    questionArea.removeChild(questionArea.childNodes[0]);
+    while(answerList.firstChild){
+        answerList.removeChild(answerList.lastChild);
+    }
+
     var questionText = document.createElement("p");
     questionText.textContent = questions[currentQuestionIndex].question;
     questionArea.appendChild(questionText);
-    for (var i = 0; i < questions[currentQuestionIndex].choices.length; i++) {
 
+    for (var i = 0; i < questions[currentQuestionIndex].choices.length; i++) {
         var listOption = document.createElement("li");
         listOption.textContent = questions[currentQuestionIndex].choices[i];
         answerList.appendChild(listOption);
